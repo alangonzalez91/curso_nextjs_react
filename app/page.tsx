c57dae429cfb51ff7bdfc6f348523eb714eff2a7
alangonzalez91 <alangonzalez91@gmail.com>
2025-12-12 19:51:07 -0300

    Lazy actualizado

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,27 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
-      <LazyReveal>
+
       <div className="relative place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <p className="m-10 text-center font-bold text-3xl">
-          ALAN GONZÁLEZ PORTAFOLIO
+          DEV ALAN GONZÁLEZ
         </p>
-        <p className="text-2xl fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-200/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="text-2xl font-mono font-bold"> E-mail: alangonzalez91@gmail.com</code>
-        </p>        
-        <p className="m-20 text-center font-bold text-2xl">
-          DESARROLLO WEB - PROGRAMAS WINDOWS - APPS ANDROID
-        </p>        
+        <LazyReveal>
+          <p className="text-2xl fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-200/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <code className="text-2xl font-mono font-bold"> E-mail: alangonzalez91@gmail.com</code>
+          </p>        
+          <p className="m-20 text-center font-bold text-2xl">
+            DESARROLLO WEB - PROGRAMAS WINDOWS - APPS ANDROID
+          </p>        
+        </LazyReveal>
       </div>
-      </LazyReveal>
 
-      <LazyReveal>
+
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <p className="mb-10 text-left font-bold text-2xl">
+        <p className="mb-10 text-align-left font-bold text-2xl">
           Desarrollo WEB
         </p>   
       </div>
-      </LazyReveal>
 
       <LazyReveal>
       <div className="reveal  mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -80,13 +80,12 @@ export default function Home() {
       </div>
       </LazyReveal>
 
-      <LazyReveal>
       <div className="m-20 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <p className="mb-10 text-left font-bold text-2xl">
           Proyectos WEB
         </p>   
       </div>
-      </LazyReveal>     
+
       <LazyReveal>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -158,13 +157,13 @@ export default function Home() {
         </a>            
       </div>                   
       </LazyReveal>     
-      <LazyReveal>
+
       <div className="m-20 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <p className="mb-10 text-left font-bold text-2xl">
           Programas Windows
         </p>   
       </div>
-      </LazyReveal>      
+
       <LazyReveal>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
@@ -211,13 +210,13 @@ export default function Home() {
         </div>
       </LazyReveal>        
  
-      <LazyReveal>
+
       <div className="m-20 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <p className="mb-10 text-left font-bold text-2xl">
           Proyectos Windows
         </p>   
       </div>
-      </LazyReveal>
+
 
       <LazyReveal>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -240,13 +239,13 @@ export default function Home() {
       </div>         
       </LazyReveal>
 
-      <LazyReveal>
+
       <div className="m-20 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <p className="mb-10 text-left font-bold text-2xl">
           Apps Android
         </p>   
       </div>
-      </LazyReveal>
+
 
       <LazyReveal>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -269,13 +268,12 @@ export default function Home() {
       </div>
       </LazyReveal>
 
-      <LazyReveal>
+
       <div className="m-20 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <p className="mb-10 text-left font-bold text-2xl">
           Proyectos Android
         </p>   
       </div>
-      </LazyReveal>
 
       <LazyReveal>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
